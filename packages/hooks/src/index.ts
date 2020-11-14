a9d176e61cc303fc08a0430db1b7ea63f7af2b5f
聂海锋 <niexiaofei@yahoo.com>
2020-11-15 02:27:29 +0800

    feat: 增加 hooks

---
+++ packages/hooks/src/index.ts
@@ -0,0 +1,5 @@
+export { default as useDetail } from './useDetail';
+export { default as useInterval } from './useInterval';
+export { default as useTimeout } from './useTimeout';
+export { default as useQuery } from './useQuery';
+export { default as useUpdate } from './useUpdate';
