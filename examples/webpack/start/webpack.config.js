47dc8097b4b09a28ed138219349b14f72d84cd46
聂海锋 <niexiaofei@yahoo.com>
2022-02-03 23:39:33 +0800

    feat: 通过html-webpack-plugin 插件自动生成 html

--- examples/webpack/start/webpack.config.js
+++ examples/webpack/start/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/main.js",
+  mode: 'production',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      publicPath: './'
+    })
+  ]
 };
